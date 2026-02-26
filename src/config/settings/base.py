15800526fe9b54a96117fceb3e6628ea10b6a503
Danil Fedorov <neweternalicon@gmail.com>
2026-02-26 12:42:26 +0300

    fixed config

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "0"
 
-ALLOWED_HOSTS = ["beatvault.online", "www.beatvault.online"]
+ALLOWED_HOSTS = ["beatvault.ru", "www.beatvault.ru", "127.0.0.1"]
 
 # Application definition
 
