1abe08c37dc8ad1f52acf84c79bd8dae21e81903
Danil Fedorov <neweternalicon@gmail.com>
2026-02-27 00:13:14 +0300

    added cron

--- src/config/settings/celery.py
+++ src/config/settings/celery.py
@@ -1,5 +1,7 @@
 import os
 
+from celery.schedules import crontab
+
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get(
     "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
@@ -8,6 +10,6 @@ CELERY_RESULT_BACKEND = os.environ.get(
 CELERY_BEAT_SCHEDULE = {
     "run-my-task-every-minute": {
         "task": "apps.vds.tasks.remove_user_keys_daily",
-        "schedule": 60.0,
+        'schedule': crontab(hour=6, minute=0),
     },
 }
