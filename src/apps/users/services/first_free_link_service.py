181d0ceaf8efaca8e9721112808848ca94c26d7c
Danil Fedorov <neweternalicon@gmail.com>
2026-03-06 15:40:48 +0300

    edited response days

--- src/apps/users/services/first_free_link_service.py
+++ src/apps/users/services/first_free_link_service.py
@@ -54,7 +54,7 @@ class FirstFreeLinkService:
 
         return {
             "link": mtproto_key.get_proxy_link(),
-            "days": (expired_date - timezone.now()).days,
+            "days": (expired_date - timezone.now()).days + 1,
         }
 
 
