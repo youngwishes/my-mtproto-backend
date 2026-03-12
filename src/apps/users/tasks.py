b7e1be09d0d103fbbdcb47a32b3fadb119c5a22d
Danil Fedorov <neweternalicon@gmail.com>
2026-03-12 22:22:43 +0300

    refactor free link user task

--- src/apps/users/tasks.py
+++ src/apps/users/tasks.py
@@ -11,32 +11,6 @@ from apps.vds.models import MTPRotoKey
 
 
 @shared_task
-def send_free_link_to_user_task(telegram_id: str):
-    with transaction.atomic():
-        user = SystemUser.objects.get(username=telegram_id)
-        MTPRotoKey.objects.filter(user=user).delete()
-        link = get_first_free_link_service()(username=telegram_id).link
-        if not link:
-            raise ValueError("Link not found. Link = %s" % link)
-
-        text = (
-            "✨ <b>Привет!</b>\n\n"
-            "🔥 Мы обновили наш сервис и сгенерировали для тебя новую ссылку <b>на 1 месяц</b> \n\n"
-            "⚡️ Подключайся и проверяй — скорость теперь должна быть стабильной!\n\n"
-            "👀 Пожалуйста, подпишись на канал @mtproto_keys — там вся информация по развитию проекта\n\n"
-            "👇 <b>Твоя ссылка:</b>"
-        )
-
-        TelegramBot.send_message_with_link(
-            text=text,
-            link=link,
-            chat_id=telegram_id,
-        )
-        user.first_month_free_used = True
-        user.save(update_fields=["first_month_free_used"])
-
-
-@shared_task
 def send_invite_to_chat_task() -> None:
     users = SystemUser.objects.filter(first_month_free_used=True).values_list(
         "username", flat=True
