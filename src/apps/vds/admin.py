182b90c6c972ae19e2736d76e4ff143115030931
Danil Fedorov <neweternalicon@gmail.com>
2026-04-02 20:02:16 +0300

    added referral wins logic

--- src/apps/vds/admin.py
+++ src/apps/vds/admin.py
@@ -48,7 +48,7 @@ class VDSInstanceAdmin(admin.ModelAdmin):
 class MTPRotoKeyAdmin(admin.ModelAdmin):
     list_select_related = ["user", "vds"]
     list_display = ["pk", "__str__", "telegram_username_link", "vds", "expired_date", "is_winner"]
-    list_filter = ["vds", "was_deleted", "is_active", "is_winner"]
+    list_filter = ["vds", "was_deleted", "is_active", "is_winner", "user_notified"]
 
     search_fields = ("user__telegram_username", "user__username")
 
