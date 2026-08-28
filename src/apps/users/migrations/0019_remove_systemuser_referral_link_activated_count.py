62445910dcdde71b0f9aa8fa0a0156a73bbd6d4d
Danil Fedorov <mysc1@yandex.ru>
2026-08-28 22:48:39 +0300

    fix(migrations): preserve referral rollback compatibility

--- src/apps/users/migrations/0019_remove_systemuser_referral_link_activated_count.py
+++ src/apps/users/migrations/0019_remove_systemuser_referral_link_activated_count.py
@@ -1,4 +1,4 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -7,8 +7,22 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
+        migrations.AlterField(
             model_name="systemuser",
             name="referral_link_activated_count",
+            field=models.PositiveSmallIntegerField(
+                db_default=0,
+                default=0,
+                verbose_name="количество активированных реф. ссылок",
+            ),
+        ),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[],
+            state_operations=[
+                migrations.RemoveField(
+                    model_name="systemuser",
+                    name="referral_link_activated_count",
+                ),
+            ],
         ),
     ]
